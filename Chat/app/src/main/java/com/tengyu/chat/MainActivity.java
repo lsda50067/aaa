f420d5c460780d45548a8a803d8c57849ee4dc58
YU\lsda1 <lsda10108017@gamil.com>
2016-12-24 00:09:01 +0800

    set TextView Content

--- Chat/app/src/main/java/com/tengyu/chat/MainActivity.java
+++ Chat/app/src/main/java/com/tengyu/chat/MainActivity.java
@@ -2,12 +2,27 @@ package com.tengyu.chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tx1 = (TextView)findViewById(R.id.TextView1);
+        TextView tx2 = (TextView)findViewById(R.id.TextView2);
+
+        // set textview
+        tx1.setText("This is test");
+        // set url link text
+        tx2.setText("https://www.google.com.tw/");
+        tx2.setAutoLinkMask(Linkify.WEB_URLS);
+        tx2.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
